834321f6813106c4cf602abe77fc0d3dccc6055b
ko-he- <koheiyatsushiro@yahoo.co.jp>
2016-05-23 23:45:45 +0900

    fix chaincode01

--- example01/chaincode_example_01.go
+++ example01/chaincode_example_01.go
@@ -1,8 +1,10 @@
-/*
- https://github.com/IBM-Blockchain/marbles-chaincode/blob/master/hyperledger/part2/part2_chaincode.go
- */
-
 package main
+
+//WARNING - this chaincode's ID is hard-coded in chaincode_example04 to illustrate one way of
+//calling chaincode from a chaincode. If this example is modified, chaincode_example04.go has
+//to be modified as well with the new ID of chaincode_example02.
+//chaincode_example05 show's how chaincode ID can be passed in as a parameter instead of
+//hard-coding.
 
 import (
         "errors"
@@ -102,3 +104,4 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
             fmt.Printf("Error starting Simple chaincode: %s", err)
         }
     }
+    Status API Training Shop Blog About
