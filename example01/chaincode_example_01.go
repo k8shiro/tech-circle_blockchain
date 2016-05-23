277485eacb3c5eb26f2c03eb67e6bbdfe15c0171
ko-he- <koheiyatsushiro@yahoo.co.jp>
2016-05-23 17:52:11 +0900

    fix chaincode01

--- example01/chaincode_example_01.go
+++ example01/chaincode_example_01.go
@@ -42,66 +42,9 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
     }
     
     fmt.Printf("Aval = %d,\n", Aval)
-    
-    //
     err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
     if err != nil {
         return nil, err
     }
     return nil, nil
-}
-    
-    
-    
-func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-    var A string
-    var Aval int
-    var err error
-    
-    if len(args) != 2 {
-        return nil, errors.New("Incorrect number of arguments. Expecting 2")
-    }
-    
-    A = args[0]
-    Aval, err = strconv.Atoi(args[1])
-    if err != nil {
-        return nil, errors.New("Expecting integer value for asset holding")
-    }
-    
-    //
-    err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
-    if err != nil {
-        return nil, err
-    }
-    
-        return nil, nil
-}
-
-
-func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-    if function != "query" {
-        return nil, errors.New("Invalid query function name. Expecting \"query\"")
-    }
-    var A string
-    var err error
-    
-    if len(args) != 1 {
-        return nil, errors.New("Incorrect number of arguments. Expecting name of the person to query")
-    }
-    
-    A = args[0]
-    
-    Avalbytes, err := stub.GetState(A)
-    if err != nil {
-        jsonResp := "{\"Error\":\"Failed to get state for " + A + "\"}"
-        return nil, errors.New(jsonResp)
-    }
-    
-    if Avalbytes == nil {
-        jsonResp := "{\"Error\":\"Nil amount for " + A + "\"}"
-        return nil, errors.New(jsonResp)
-    }
-    
-    message =Avalbytes
-    return  []byte(message), nil
 }
