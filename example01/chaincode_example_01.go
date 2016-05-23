4a1a74ddb8a8bf0d6d76a6b74880a42536d10198
ko-he- <koheiyatsushiro@yahoo.co.jp>
2016-05-23 17:55:55 +0900

    fix chaincode01

--- example01/chaincode_example_01.go
+++ example01/chaincode_example_01.go
@@ -1,38 +1,30 @@
-
+/*
+ https://github.com/IBM-Blockchain/marbles-chaincode/blob/master/hyperledger/part2/part2_chaincode.go
+ */
 
 package main
 
 import (
-    "errors"
-    "fmt"
-    "strconv"
-    "github.com/hyperledger/fabric/core/chaincode/shim"
-)
+        "errors"
+        "fmt"
+        "strconv"
+        "github.com/hyperledger/fabric/core/chaincode/shim"
+        )
 
-
+// SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
 }
 
-// ============================================================================================================================
-// Main
-// ============================================================================================================================
-
-func main() {
-    err := shim.Start(new(SimpleChaincode))
-    if err != nil {
-        fmt.Printf("Error starting Simple chaincode: %s", err)
-    }
-}
-
 func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-    var A string
-    var Aval int
+    var A string    // Entities
+    var Aval int // Asset holdings
     var err error
     
     if len(args) != 2 {
         return nil, errors.New("Incorrect number of arguments. Expecting 4")
     }
     
+    // Initialize the chaincode
     A = args[0]
     Aval, err = strconv.Atoi(args[1])
     if err != nil {
@@ -40,33 +32,30 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
     }
     
     fmt.Printf("Aval = %d,\n", Aval)
+    
+    // Write the state to the ledger
     err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
     if err != nil {
         return nil, err
     }
     return nil, nil
-}
-
+    }
     
-    
-    
+    // Transaction makes payment of X units from A to B
     func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-        var A string
-        var Aval int
+        var A string    // Entities
         var err error
+        
+        
         
         if len(args) != 2 {
             return nil, errors.New("Incorrect number of arguments. Expecting 2")
         }
         
         A = args[0]
-        Aval, err = strconv.Atoi(args[1])
-        if err != nil {
-            return nil, errors.New("Expecting integer value for asset holding")
-        }
         
-        //
-        err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
+        // Write the state back to the ledger
+        err = stub.PutState(A, []byte(args[1]))
         if err != nil {
             return nil, err
         }
@@ -75,11 +64,13 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
     }
     
     
+    
+    // Query callback representing the query of a chaincode
     func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
         if function != "query" {
             return nil, errors.New("Invalid query function name. Expecting \"query\"")
         }
-        var A string
+        var A string // Entities
         var err error
         
         if len(args) != 1 {
@@ -88,6 +79,7 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
         
         A = args[0]
         
+        // Get the state from the ledger
         Avalbytes, err := stub.GetState(A)
         if err != nil {
             jsonResp := "{\"Error\":\"Failed to get state for " + A + "\"}"
@@ -99,6 +91,14 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
             return nil, errors.New(jsonResp)
         }
         
-        message =Avalbytes
-        return  []byte(message), nil
+        jsonResp := "{\"Name\":\"" + A + "\",\"Amount\":\"" + string(Avalbytes) + "\"}"
+        fmt.Printf("Query Response:%s\n", jsonResp)
+        return Avalbytes, nil
+    }
+    
+    func main() {
+        err := shim.Start(new(SimpleChaincode))
+        if err != nil {
+            fmt.Printf("Error starting Simple chaincode: %s", err)
+        }
     }
