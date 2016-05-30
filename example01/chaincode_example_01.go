921346a1cc9d22dafb5782ce382265d8dd166b78
ko-he- <koheiyatsushiro@yahoo.co.jp>
2016-05-30 23:38:46 +0900

    fix

--- example01/chaincode_example_01.go
+++ example01/chaincode_example_01.go
@@ -3,14 +3,22 @@ package main
 import (
         "errors"
         "fmt"
-        "strconv"
         
         "github.com/hyperledger/fabric/core/chaincode/shim"
         )
 
+// SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
 }
 
+func main() {
+    err := shim.Start(new(SimpleChaincode))
+    if err != nil {
+        fmt.Printf("Error starting Simple chaincode: %s", err)
+    }
+}
+
+// Init resets all the things
 func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
     if len(args) != 1 {
         return nil, errors.New("Incorrect number of arguments. Expecting 1")
@@ -24,52 +32,69 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
     return nil, nil
 }
 
+// Invoke isur entry point to invoke a chaincode function
 func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-    if len(args) != 1 {
-        return nil, errors.New("Incorrect number of arguments. Expecting 1")
+    fmt.Println("invoke is running " + function)
+    
+    // Handle different functions
+    if function == "init" {
+        return t.Init(stub, "init", args)
+    } else if function == "write" {
+        return t.write(stub, args)
+    }
+    fmt.Println("invoke did not find func: " + function)
+    
+    return nil, errors.New("Received unknown function invocation")
+}
+
+// Query is our entry point for queries
+func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+    fmt.Println("query is running " + function)
+    
+    // Handle different functions
+    if function == "read" {                            //read a variable
+        return t.read(stub, args)
+    }
+    fmt.Println("query did not find func: " + function)
+    
+    return nil, errors.New("Received unknown function query")
+}
+
+// write - invoke function to write key/value pair
+func (t *SimpleChaincode) write(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+    var name, value string
+    var err error
+    fmt.Println("running write()")
+    
+    if len(args) != 2 {
+        return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the variable and value to set")
     }
     
-    err := stub.PutState("hello_world", []byte(args[0]))
+    name = args[0]                            //rename for funsies
+    value = args[1]
+    err = stub.PutState(name, []byte(value))  //write the variable into the chaincode state
     if err != nil {
         return nil, err
     }
-    
     return nil, nil
 }
 
-
-func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-    if function != "query" {
-        return nil, errors.New("Invalid query function name. Expecting \"query\"")
-    }
-    var itemId string
+// read - query function to read key/value pair
+func (t *SimpleChaincode) read(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+    var name, jsonResp string
     var err error
     
     if len(args) != 1 {
-        return nil, errors.New("Incorrect number of arguments. Expecting name of the person to query")
+        return nil, errors.New("Incorrect number of arguments. Expecting name of the var to query")
     }
     
-    itemId = args[0]
-    
-    itemBytes, err := stub.GetState(itemId)
+    name = args[0]
+    valAsbytes, err := stub.GetState(name)
     if err != nil {
-        jsonResp := "{\"Error\":\"Failed to get state for " + itemId + "\"}"
+        jsonResp = "{\"Error\":\"Failed to get state for " + name + "\"}"
         return nil, errors.New(jsonResp)
     }
     
-    if itemBytes == nil {
-        jsonResp := "{\"Error\":\"Nil amount for " + itemId + "\"}"
-        return nil, errors.New(jsonResp)
-    }
-    
-    jsonResp := "{\"Name\":\"" + itemId + "\",\"Amount\":\"" + string(itemBytes) + "\"}"
-    fmt.Printf("Query Response:%s\n", jsonResp)
-    return itemBytes, nil
+    return valAsbytes, nil
 }
-
-func main() {
-    err := shim.Start(new(SimpleChaincode))
-    if err != nil {
-        fmt.Printf("Error starting Simple chaincode: %s", err)
-    }
-}
+Status API Training Shop Blog About
