bcdc1245271e19851aadad56dbd0532b04b7ac07
ko-he- <koheiyatsushiro@yahoo.co.jp>
2016-05-31 09:25:34 +0900

    fix code

--- example01/chaincode_example_01.go
+++ example01/chaincode_example_01.go
@@ -69,7 +69,7 @@ func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args 
         return nil, errors.New(jsonResp)
     }
     
-    jsonResp := "{\"Name\":\"" + A + "\",\"Amount\":\"" + string(valbytes) + "\"}"
+    jsonResp := "{\"Name\":\"" + key + "\",\"Amount\":\"" + string(valbytes) + "\"}"
     fmt.Printf("Query Response:%s\n", jsonResp)
     return valbytes, nil
 }
