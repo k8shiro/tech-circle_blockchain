63a7113645805cefd83e59b392b5ed83ab307016
ko-he- <koheiyatsushiro@yahoo.co.jp>
2016-05-31 09:19:15 +0900

    fix code

--- example01/chaincode_example_01.go
+++ example01/chaincode_example_01.go
@@ -20,7 +20,7 @@ type SimpleChaincode struct {
 
 func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
     var A, B string    // Entities
-    var Aval, Bval int // Asset holdings
+    var Aval, Bval string // Asset holdings
     var err error
     
     if len(args) != 4 {
@@ -29,24 +29,24 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
     
     // Initialize the chaincode
     A = args[0]
-    Aval, err = strconv.Atoi(args[1])
+    Aval= args[1]
     if err != nil {
         return nil, errors.New("Expecting integer value for asset holding")
     }
     B = args[2]
-    Bval, err = strconv.Atoi(args[3])
+    Bval = args[3]
     if err != nil {
         return nil, errors.New("Expecting integer value for asset holding")
     }
-    fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
+
     
     // Write the state to the ledger
-    err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
+    err = stub.PutState(A, []byte(Aval))
     if err != nil {
         return nil, err
     }
     
-    err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
+    err = stub.PutState(B, []byte(Bval))
     if err != nil {
         return nil, err
     }
@@ -56,56 +56,34 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 
 // Transaction makes payment of X units from A to B
 func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-    if function == "delete" {
-        // Deletes an entity from its state
-        return t.delete(stub, args)
-    }
-    
     var A, B string    // Entities
-    var Aval, Bval int // Asset holdings
-    var X int          // Transaction value
+    var Aval, Bval string // Asset holdings
     var err error
     
-    if len(args) != 3 {
-        return nil, errors.New("Incorrect number of arguments. Expecting 3")
+    if len(args) != 4 {
+        return nil, errors.New("Incorrect number of arguments. Expecting 4")
     }
     
+    // Initialize the chaincode
     A = args[0]
-    B = args[1]
-    
-    // Get the state from the ledger
-    // TODO: will be nice to have a GetAllState call to ledger
-    Avalbytes, err := stub.GetState(A)
+    Aval= args[1]
     if err != nil {
-        return nil, errors.New("Failed to get state")
+        return nil, errors.New("Expecting integer value for asset holding")
     }
-    if Avalbytes == nil {
-        return nil, errors.New("Entity not found")
-    }
-    Aval, _ = strconv.Atoi(string(Avalbytes))
-    
-    Bvalbytes, err := stub.GetState(B)
+    B = args[2]
+    Bval = args[3]
     if err != nil {
-        return nil, errors.New("Failed to get state")
+        return nil, errors.New("Expecting integer value for asset holding")
     }
-    if Bvalbytes == nil {
-        return nil, errors.New("Entity not found")
-    }
-    Bval, _ = strconv.Atoi(string(Bvalbytes))
     
-    // Perform the execution
-    X, err = strconv.Atoi(args[2])
-    Aval = Aval - X
-    Bval = Bval + X
-    fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
     
-    // Write the state back to the ledger
-    err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
+    // Write the state to the ledger
+    err = stub.PutState(A, []byte(Aval))
     if err != nil {
         return nil, err
     }
     
-    err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
+    err = stub.PutState(B, []byte(Bval))
     if err != nil {
         return nil, err
     }
@@ -113,22 +91,7 @@ func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args
     return nil, nil
 }
 
-// Deletes an entity from state
-func (t *SimpleChaincode) delete(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
-    if len(args) != 1 {
-        return nil, errors.New("Incorrect number of arguments. Expecting 1")
-    }
-    
-    A := args[0]
-    
-    // Delete the key from the state in ledger
-    err := stub.DelState(A)
-    if err != nil {
-        return nil, errors.New("Failed to delete state")
-    }
-    
-    return nil, nil
-}
+
 
 // Query callback representing the query of a chaincode
 func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
