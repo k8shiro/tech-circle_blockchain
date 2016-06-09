fc02be1c76d035ac37f7069bae4bbc18e7dec315
ko-he- <koheiyatsushiro@yahoo.co.jp>
2016-06-09 17:40:49 +0900

    add

--- example06/chaincode_example_06.go
+++ example06/chaincode_example_06.go
@@ -15,7 +15,7 @@ type SimpleChaincode struct {
 var myLogger = logging.MustGetLogger("asset_mgm")
 
 func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-    fmt.printf("{\"Error\": \"ssssssss\"}")
+    fmt.Printf("{\"Error\": \"ssssssss\"}")
     
     if len(args) != 0 {
         return nil, errors.New("Incorrect number of arguments. Expecting 0")
